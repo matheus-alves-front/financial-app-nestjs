f1a80736f216b1df63b62fd4b2d414b3d502871c
Matheus <matheus.alves.front.m2@gmail.com>
2023-06-14 03:08:46 -0300

    relation category expense ok

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaRelations } from 'src/prisma/relations.service';
 import { CategoryService } from './category.service';
 import { Category } from '@prisma/client';
 
@@ -7,7 +8,8 @@ import { Category } from '@prisma/client';
 export class CategoryController {
   constructor(
     private readonly prisma: PrismaService,
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private prismaRelations: PrismaRelations
   ) {}
 
   @Post()
@@ -15,9 +17,28 @@ export class CategoryController {
     @Param('profileId') profileId: string,
     @Body() createCategory: Category 
   ) {
+    const profileIdNumber = Number(profileId)
     const { name: categoryName } = createCategory
     const categoryCreated = await this.categoryService.createCategory(categoryName)
 
+    await this.prismaRelations.addCategoryRelation(profileIdNumber, categoryCreated?.id)
+
     return categoryCreated
+  }
+
+  @Get() 
+  async findAllCategories(@Param('profileId') profileId: string) {
+    const profileIdNumber = Number(profileId)
+    const categories = this.categoryService.findAllCategories(profileIdNumber)
+
+    return categories
+  }
+
+  @Get('/expenses')
+  async findAllCategoriesWithProducts(@Param('profileId') profileId: string) {
+    const profileIdNumber = Number(profileId)
+    const categoriesWithProducts = await this.categoryService.findAllCategoriesWithExpenses(profileIdNumber)
+  
+    return categoriesWithProducts
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,7 +1,16 @@
 import { Injectable, Param } from '@nestjs/common';
+import { Expense } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaRelations } from 'src/prisma/relations.service';
 import { DateService } from 'src/utils/getDate.service';
+
+export interface categoryExpenses {
+  category: {
+    id: number
+    name: string
+    expenses: Expense[]
+  }
+}
 
 @Injectable()
 export class CategoryService {
@@ -19,5 +28,52 @@ export class CategoryService {
     })
 
     return categoryCreated
+  }
+
+  async findAllCategories(profileId: number) {
+    const allCategoriesExpenses = await this.prisma.categoryExpense.findMany({
+      where: {
+        profileId
+      },
+      select: {
+        category: true
+      },
+      distinct: ['categoryId']
+    })
+
+    const categories = allCategoriesExpenses.map((item) => item.category);
+
+    return categories || false
+  }
+
+  async findAllCategoriesWithExpenses(profileId: number) {
+    const getAllCategoriesExpenses = await this.prisma.categoryExpense.findMany({
+      where: {
+        profileId
+      },
+      select: {
+        category: true,
+        expense: true
+      }
+    })
+
+    const groupedCategoriesWithExpenses = getAllCategoriesExpenses.reduce((acc, { category, expense }) => {
+      const existingCategory = acc.find((item) => item.id === category.id)
+  
+      if (existingCategory) {
+        existingCategory.expenses.push(expense)
+      } else {
+        const newCategory = {
+          id: category.id,
+          name: category.name,
+          expenses: [expense]
+        };
+        acc.push(newCategory)
+      }
+  
+      return acc
+    }, []);
+  
+    return groupedCategoriesWithExpenses || false
   }
 }
